5ea374050b2d645c1957688d7dc4cefccd3858f0
adrien sellegit config --global user.name adrien <selleadrien@gmail.com>
2023-11-25 16:48:49 +0100

    correction du 25/11/2023 à 16:48

--- one.js
+++ one.js
@@ -3,11 +3,13 @@ document.addEventListener( 'DOMContentLoaded', function() {
     var splide = new Splide( '.splide',{
         pagination: false,
         arrowPath: false,
+        arrows: false,
         type: 'loop',
         autoplay: true,
         start: true,
-        perPage: 3,
-        width: '100%'
+        perPage: 5,
+        width: '100%',
     } );
     splide.mount();
 } );
+
